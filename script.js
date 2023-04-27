887ea8021caaaf78b5f8833d79a3a19428cee702
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-27 11:31:57 +0200

    Add absolute header positioning

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const sidenav = document.querySelector(".side-nav");
 const btn = document.querySelector(".hamburger-btn");
+const backg = document.querySelector(".background");
 
 btn.addEventListener("click", () => {
   sidenav.classList.toggle("show");
+  backg.classList.toggle("dark");
 });
