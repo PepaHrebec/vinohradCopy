aea0375800990466bba65a331af7f165a79ea38f
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-29 18:39:04 +0200

    Add hamburger icon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const sidenav = document.querySelector(".side-nav");
-const btn = document.querySelector(".hamburger-btn");
+const btn = document.querySelector(".hamburger");
 const backg = document.querySelector(".background");
 
 btn.addEventListener("click", () => {
+  btn.classList.toggle("is-active");
   sidenav.classList.toggle("show");
   backg.classList.toggle("dark");
 });
